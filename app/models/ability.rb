9fa726314fc810dac1b71555e595b4b63c3b6f22
Anhad Gill <anhadgill@Anhads-MacBook-Pro.local>
2018-07-19 15:28:00 -0700

    Adds some part of authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,11 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
       user ||= User.new # guest user (not logged in)
-      if user.admin?
+      if user.role === 1
+        user.role = :admin
         can :manage, :all
+      elsif user.role === 2
+        user.role = :manager
       else
         can :read, :all
       end
